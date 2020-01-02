307900db1d2ca28fa9c5676404619a3bf66052c0
Adam Romero <adamromero89@gmail.com>
2020-01-01 22:40:44 -0800

    Edit server.js

--- server.js
+++ server.js
@@ -97,8 +97,10 @@ app.post("/charge", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
+   app.use(express.static(path.join(__dirname, "build")));
+
    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "/client/public/index.html"));
+      res.sendFile(path.join(__dirname, "build", "index.html"));
    });
 }
 
