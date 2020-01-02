deaaf73cb73b05ee99fe15dc95176a505162a46e
Adam Romero <adamromero89@gmail.com>
2020-01-01 22:01:56 -0800

    Serve the html file

--- server.js
+++ server.js
@@ -93,4 +93,13 @@ app.post("/charge", (req, res) => {
       .then(charge => res.render("/"));
 });
 
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+
+   const path = require("path");
+   app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+   });
+}
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
