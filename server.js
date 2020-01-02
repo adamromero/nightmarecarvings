31aa8d3ad63f0136f04d3b81fd5b5b4405c44a6a
Adam Romero <adamromero89@gmail.com>
2020-01-01 22:22:20 -0800

    Edit server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "../client/public")));
+app.use(express.static(path.join(__dirname, "/client/public")));
 
 app.get("/api/patterns", (req, res) => {
    const patterns = [
